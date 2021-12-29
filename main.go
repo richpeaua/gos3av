fb084a21dac08d91824b5e93f1b21b44202ddb1c
richpeaua <rich.peaua@gmail.com>
2021-12-28 22:39:13 -1000

    helloworld: testScenario 3 - expectedErrNameLen; pass

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ import (
 func helloName(n string) (string, error) {
 	greeting := fmt.Sprintf("Hello, %s!", n)
 
-	switch n {
-	case "":
-		return greeting, errors.New("`name` cannot be blank")
+	switch {
+	case n == "":
+		return greeting, errors.New("name cannot be blank")
+	case len(n) > 64:
+		return greeting, errors.New("name cannot exceed 64 chars")
 	}
-
 	return greeting, nil
 }
 
