2a06714a13e9b6e16b0384597707d584c7a70e87
richpeaua <rich.peaua@gmail.com>
2021-12-28 22:51:04 -1000

    scanner dev start

--- main.go
+++ main.go
@@ -1,28 +1,3 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
-
-func helloName(n string) (string, error) {
-	greeting := fmt.Sprintf("Hello, %s!", n)
-
-	switch {
-	case n == "":
-		return greeting, errors.New("name cannot be blank")
-	case len(n) > 64:
-		return greeting, errors.New("name cannot exceed 64 chars")
-	}
-	return greeting, nil
-}
-
-func main() {
-	name     := ""
-	greeting, err := helloName(name)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(greeting)
-}
+func main() {}
